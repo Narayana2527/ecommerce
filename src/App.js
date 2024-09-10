1a6a3e06b97c60ed28052c1b753bc67d9151ddf2
Narayana2527 <avulanarayana.2016@gmail.com>
2024-09-10 21:44:54 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import React,{useEffect, useState} from "react"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.js'
-import { Outlet } from 'react-router-dom'
-import Navbar from './components/navbar'
-import Sidebar from './components/sidebar'
-import Footer from './components/footer'
+import React from "react"
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/js/bootstrap.bundle.js'
+// import { Outlet } from 'react-router-dom'
+// import Navbar from './components/navbar'
+// import Sidebar from './components/sidebar'
+// import Footer from './components/footer'
 
 const App = () => {
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isMobile,setIsMobile] = useState(window.innerWidth < 768);
+  // const [isMobile,setIsMobile] = useState(window.innerWidth < 768);
 
-  useEffect(()=>{
-    const handleResize =()=>{
-      setIsMobile(window.innerWidth < 768)
+  // useEffect(()=>{
+  //   const handleResize =()=>{
+  //     setIsMobile(window.innerWidth < 768)
       
-    };
-    window.addEventListener('resize',handleResize)
-    return()=>{
-      window.removeEventListener('resize',handleResize)
-    }
-  },[])
+  //   };
+  //   window.addEventListener('resize',handleResize)
+  //   return()=>{
+  //     window.removeEventListener('resize',handleResize)
+  //   }
+  // },[])
   
   return (
-    <>
-      {isMobile ? (
-        <Sidebar  />
-      ) : (
-        <Navbar  />
-      )}
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </>
+    
+      // {isMobile ? (
+      //   <Sidebar  />
+      // ) : (
+      //   <Navbar  />
+      // )}
+      // <main>
+      //   <Outlet />
+      // </main>
+      // <Footer />
+    <div>
+      <h1>Welcome Nani</h1>
+    </div>
   )
 }
 
